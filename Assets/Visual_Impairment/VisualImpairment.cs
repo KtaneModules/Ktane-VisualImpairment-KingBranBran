d651063089fb382173e984f29212aa923283c8a6
KingBranBran <kingbran4521@gmail.com>
2018-01-20 21:59:17 -0800

    - Added logging and Twitch Plays support - Fixed shaders - Changed the rotation logic - Cleaned up script - Removed a few unused files

--- Assets/Visual_Impairment/VisualImpairment.cs
+++ Assets/Visual_Impairment/VisualImpairment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
@@ -24,82 +25,94 @@ public class VisualImpairment : MonoBehaviour
     private int Blue;
     private int Green;
     private int Red;
-    private int correctButtons = 0;
-    private int correctButtonsPressed = 0;
-    private bool firstPress;
-    private HashSet<int> buttonsPressed;
+    private bool anyPressed;
+    private HashSet<int> buttonsLeftToPress;
     private int roundsFinished = 0;
     private bool moduleSolved = false;
     private int stageCount;
     public GameObject buttonGroup;
-    private bool flip;
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
         stageCount = Random.Range(1, 4);
-        pictureNum = Random.Range(0, 8);
-        orientation = Random.Range(1, 9);
-        color = Random.Range(0, 4);
-        PickPicture();
-        impairment.OnActivate += Activate;
-        White = Random.Range(3, 7);
-        Blue = Random.Range(3, 7);
-        Green = Random.Range(3, 7);
-        Red = Random.Range(3, 7);
-        buttonsPressed = new HashSet<int>();
-        flip = orientation > 4;
+
+        DebugLog("This module will have {0} stages.", stageCount);
+        DebugLog("Start of stage #1.");
+
+        impairment.OnActivate += ResetModule;
+        buttonsLeftToPress = new HashSet<int>();
 
         for (int i = 0; i < buttons.Length; i++)
-        {
             buttons[i].OnInteract += ButtonPressed(i);
-        }
-
     }
 
-
-    private KMSelectable.OnInteractHandler ButtonPressed(int i)
+    private KMSelectable.OnInteractHandler ButtonPressed(int rotated)
     {
         return delegate
         {
-            Audio.PlaySoundAtTransform("tick", buttons[i].transform);
-            if (buttonsPressed.Contains(i) || moduleSolved)
-            {
+            Audio.PlaySoundAtTransform("tick", buttons[rotated].transform);
+            if (moduleSolved)
                 return false;
-            }
-            buttonsPressed.Add(i);
-            if (firstPress == false && picture[i] == colorList[color])
-            {
-                ClearPicture();
-                firstPress = true;
-            }
+
+            var i = unrotate(rotated);
+            DebugLog("You pressed {0}{1}, which is {2}{3} in the original picture.", (char)('A' + rotated % 5), (char)('1' + rotated / 5), (char)('A' + i % 5), (char)('1' + i / 5));
             if (picture[i] == colorList[color])
             {
-                correctButtonsPressed++;
-                if (color == 0)
+                buttonsLeftToPress.Remove(i);
+                if (!anyPressed)
                 {
-                    buttons[i].gameObject.GetComponent<Renderer>().material = materials[3];
+                    ClearPicture();
+                    anyPressed = true;
                 }
-                else
-                {
-                    buttons[i].gameObject.GetComponent<Renderer>().material = materials[color - 1];
-                }
-                if (correctButtons == correctButtonsPressed)
-                {
+                buttons[rotated].gameObject.GetComponent<Renderer>().material = materials[color];
+                if (buttonsLeftToPress.Count == 0)
                     HandleRoundEnd();
-                }
-
             }
             else
             {
-                DebugLog("Button #{0} was incorrect.", i + 1);
+                DebugLog("Button was incorrect.");
                 impairment.HandleStrike();
-                StartCoroutine(BreakButtons());
+                StartCoroutine(DelayThenReset());
                 return false;
             }
 
             return false;
         };
+    }
+
+    private int unrotate(int i)
+    {
+        var x = i % 5;
+        var y = i / 5;
+        switch (orientation)
+        {
+            case 1: return i;
+            case 2: return 5 * (4 - x) + y;
+            case 3: return 5 * (4 - y) + 4 - x;
+            case 4: return 5 * x + 4 - y;
+            case 5: return 5 * y + 4 - x;
+            case 6: return 5 * (4 - x) + 4 - y;
+            case 7: return 5 * (4 - y) + x;
+            default: return 5 * x + y;
+        }
+    }
+
+    private int rotate(int i)
+    {
+        var x = i % 5;
+        var y = i / 5;
+        switch (orientation)
+        {
+            case 1: return i;
+            case 2: return 5 * x + 4 - y;
+            case 3: return 5 * (4 - y) + 4 - x;
+            case 4: return 5 * (4 - x) + y;
+            case 5: return 5 * y + 4 - x;
+            case 6: return 5 * (4 - x) + 4 - y;
+            case 7: return 5 * (4 - y) + x;
+            default: return 5 * x + y;
+        }
     }
 
     private void HandleRoundEnd()
@@ -118,25 +131,40 @@ public class VisualImpairment : MonoBehaviour
             impairment.HandlePass();
             return;
         }
-        ResetModule();
+        else
+        {
+            DebugLog("Start of stage #{0}.", roundsFinished + 1);
+            StartCoroutine(DelayThenReset());
+        }
     }
 
     private void ResetModule()
     {
-        pictureNum = Random.Range(0, 8);
-        orientation = Random.Range(1, 9);
         color = Random.Range(0, 4);
+        indicator.GetComponent<Renderer>().material = materials[color];
+
+        DebugLog("Color to press: {0}", new[] { "Blue", "Green", "Red", "White" }[color]);
+
+        var colorNumbers = Enumerable.Range(3, 4).ToList();
+        White = PickRandomFrom(colorNumbers);
+        Blue = PickRandomFrom(colorNumbers);
+        Green = PickRandomFrom(colorNumbers);
+        Red = PickRandomFrom(colorNumbers);
+
+        buttonsLeftToPress.Clear();
+        anyPressed = false;
+
         PickPicture();
-        White = Random.Range(3, 7);
-        Blue = Random.Range(3, 7);
-        Green = Random.Range(3, 7);
-        Red = Random.Range(3, 7);
-        buttonsPressed.Clear();
-        correctButtons = 0;
-        correctButtonsPressed = 0;
-        flip = orientation > 4;
-        firstPress = false;
-        Activate();
+
+        DebugLog("Buttons to press: {0}", string.Join(", ", buttonsLeftToPress.Select(ix => rotate(ix)).Select(ix => "" + (char)('A' + ix % 5) + (char)('1' + ix / 5)).OrderBy(str => str).ToArray()));
+    }
+
+    private int PickRandomFrom(List<int> list)
+    {
+        var ix = Random.Range(0, list.Count);
+        var result = list[ix];
+        list.RemoveAt(ix);
+        return result;
     }
 
     private void ClearPicture()
@@ -147,27 +175,10 @@ public class VisualImpairment : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-
-    }
-
-    void Activate()
-    {
-        SetPicture();
-        FindCorrectButtonAmount();
-        if (color == 0)
-        {
-            indicator.gameObject.GetComponent<Renderer>().material = materials[3];
-        }
-        else
-        {
-            indicator.gameObject.GetComponent<Renderer>().material = materials[color - 1];
-        }
-    }
-
     void PickPicture()
     {
+        orientation = 6;// Random.Range(1, 9);
+        pictureNum = Random.Range(0, 8);
         switch (pictureNum)
         {
             case 0:
@@ -179,7 +190,6 @@ public class VisualImpairment : MonoBehaviour
                     "B", "W", "W", "G", "B",
                     "R", "B", "W", "G", "R",
                 };
-                DebugLog("Picture 1 was chosen.");
                 break;
             case 1:
                 picture = new string[25]
@@ -190,7 +200,6 @@ public class VisualImpairment : MonoBehaviour
                     "W", "R", "G", "G", "G",
                     "R", "B", "R", "B", "G",
                 };
-                DebugLog("Picture 2 was chosen.");
                 break;
             case 2:
                 picture = new string[25]
@@ -201,7 +210,6 @@ public class VisualImpairment : MonoBehaviour
                     "R", "W", "W", "W", "B",
                     "R", "B", "W", "B", "B",
                };
-                DebugLog("Picture 3 was chosen.");
                 break;
             case 3:
                 picture = new string[25]
@@ -212,7 +220,6 @@ public class VisualImpairment : MonoBehaviour
                     "G", "W", "B", "W", "G",
                     "B", "G", "G", "G", "B",
                };
-                DebugLog("Picture 4 was chosen.");
                 break;
             case 4:
                 picture = new string[25]
@@ -223,7 +230,6 @@ public class VisualImpairment : MonoBehaviour
                     "W", "G", "G", "W", "B",
                     "B", "R", "G", "R", "G",
                };
-                DebugLog("Picture 5 was chosen.");
                 break;
             case 5:
                 picture = new string[25]
@@ -234,7 +240,6 @@ public class VisualImpairment : MonoBehaviour
                     "R", "W", "B", "G", "B",
                     "R", "W", "R", "R", "B",
                };
-                DebugLog("Picture 6 was chosen.");
                 break;
             case 6:
                 picture = new string[25]
@@ -245,7 +250,6 @@ public class VisualImpairment : MonoBehaviour
                     "W", "G", "B", "G", "B",
                     "R", "W", "G", "B", "G",
                };
-                DebugLog("Picture 7 was chosen.");
                 break;
             case 7:
                 picture = new string[25]
@@ -256,7 +260,6 @@ public class VisualImpairment : MonoBehaviour
                     "B", "G", "G", "G", "R",
                     "R", "G", "W", "G", "B",
                };
-                DebugLog("Picture 8 was chosen.");
                 break;
             case 8:
                 picture = new string[25]
@@ -267,74 +270,33 @@ public class VisualImpairment : MonoBehaviour
                     "G", "B", "G", "G", "B",
                     "W", "R", "G", "R", "B",
                };
-                DebugLog("Picture 9 was chosen.");
                 break;
         }
-    }
 
-    void SetPicture()
-    {
-        if (orientation == 1 || orientation == 5)
+        for (int i = 0; i < picture.Length; i++)
         {
-            RotatePicture(0);
-        }
-        else if (orientation == 2 || orientation == 6)
-        {
-            RotatePicture(90);
-        }
-        else if (orientation == 3 || orientation == 7)
-        {
-            RotatePicture(180);
-        }
-        else if (orientation == 4 || orientation == 8)
-        {
-            RotatePicture(270);
+            if (picture[i] == "W")
+            {
+                buttons[rotate(i)].GetComponent<Renderer>().material = materials[White];
+            }
+            else if (picture[i] == "B")
+            {
+                buttons[rotate(i)].GetComponent<Renderer>().material = materials[Blue];
+            }
+            else if (picture[i] == "G")
+            {
+                buttons[rotate(i)].GetComponent<Renderer>().material = materials[Green];
+            }
+            else if (picture[i] == "R")
+            {
+                buttons[rotate(i)].GetComponent<Renderer>().material = materials[Red];
+            }
+
+            if (picture[i] == colorList[color])
+                buttonsLeftToPress.Add(i);
         }
 
-        for (int i = 0; i < buttons.Length; i++)
-            SetColor(buttons[i], i);
-    }
-
-    private void SetColor(KMSelectable button, int index)
-    {
-        while (Blue == White)
-        {
-            Blue = Random.Range(3, 7);
-        }
-        while (Green == White || Green == Blue)
-        {
-            Green = Random.Range(3, 7);
-        }
-        while (Red == White || Red == Green || Red == Blue)
-        {
-            Red = Random.Range(3, 7);
-        }
-        if (picture[index] == "W")
-        {
-            button.GetComponent<Renderer>().material = materials[White];
-        }
-        else if (picture[index] == "B")
-        {
-            button.GetComponent<Renderer>().material = materials[Blue];
-        }
-        else if (picture[index] == "G")
-        {
-            button.GetComponent<Renderer>().material = materials[Green];
-        }
-        else if (picture[index] == "R")
-        {
-            button.GetComponent<Renderer>().material = materials[Red];
-        }
-    }
-
-    private void RotatePicture(int degrees)
-    {
-        buttonGroup.transform.localEulerAngles = new Vector3(0, degrees, flip ? 180 : 0);
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            buttons[i].Highlight.transform.localPosition = new Vector3(0, flip ? -.52f : .52f, 0);
-            buttons[i].Highlight.transform.localEulerAngles = new Vector3(0, 0, flip ? 180 : 0);
-        }
+        DebugLog("Picture {0} was chosen at rotation {1}{2}.", pictureNum + 1, ((orientation - 1) % 4) * 90, orientation >= 4 ? " and flipped" : "");
     }
 
     private void DebugLog(string log, params object[] args)
@@ -343,26 +305,40 @@ public class VisualImpairment : MonoBehaviour
         Debug.LogFormat("[Visual Impairment #{0}] {1}", _moduleId, logData);
     }
 
-
-    public void FindCorrectButtonAmount()
+    IEnumerator DelayThenReset()
     {
-        foreach (string button in picture)
-        {
-            if (button == colorList[color])
-            {
-                correctButtons++;
-            }
-        }
-        DebugLog("Number of correct buttons on this picture: {0}", correctButtons);
-    }
-
-    IEnumerator BreakButtons()
-    {
+        indicator.gameObject.GetComponent<Renderer>().material = materials[4];
         foreach (KMSelectable button in buttons)
-        {
             button.gameObject.GetComponent<Renderer>().material = materials[7];
-        }
         yield return new WaitForSeconds(1);
         ResetModule();
+    }
+
+#pragma warning disable 414
+    private string TwitchHelpMessage = @"Use “!{0} a4 b3 d2” to press the squares. Letters are columns and numbers are rows.";
+#pragma warning restore 414
+
+    private static string[] supportedTwitchCommands = new[] { "press", "click", "submit" };
+
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        var parts = command.ToLowerInvariant().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var skip = 0;
+        if (parts.Length > 0 && supportedTwitchCommands.Contains(parts[0]))
+            skip = 1;
+
+        if (parts.Length > skip && parts.Skip(skip).All(part => part.Length == 2 && "abcde".Contains(part[0]) && "12345".Contains(part[1])))
+        {
+            yield return null;
+
+            for (int i = skip; i < parts.Length; i++)
+            {
+                var x = parts[i][0] - 'a';
+                var y = parts[i][1] - '1';
+                buttons[y * 5 + x].OnInteract();
+                yield return new WaitForSeconds(.1f);
+            }
+        }
     }
 }
